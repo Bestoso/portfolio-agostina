3f60af80556bdd52c11086097ac6401b040579ab
Santiago Bestoso <santiaguitobestoso@gmail.com>
2022-10-19 14:14:09 -0300

    gh-pages

--- portfolio-agostina/src/App.jsx
+++ portfolio-agostina/src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import './loader.css';
 import 'boxicons';
 import Aos from "aos";
-import CountUp from 'react-countup';
 import { NavBar } from "./Components/Navegacion/NavBar";
 import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import { Inicio } from "./Components/NavegableItems/Inicio/Inicio";
@@ -16,55 +15,11 @@ function App() {
 
   const [loaderFirst, setLoaderFirst] = useState('notdelayed');
   const [loaderSecond, setLoaderSecond] = useState('delayed');
-  const [loader, setLoader] = useState(true);
-  const [loaderClass, setLoaderClass] = useState('loader');
-  const [loaderClass2, setLoaderClass2] = useState('loadername');
-  const loaderRef = useRef();
-  
-  const endLoader = () => {
-    setLoaderClass('loader gone');
-    setLoaderClass2('loadername gone');
-    setTimeout(() => {
-      setLoader(false);
-    }, 800)
-  }
 
   useEffect(() => {
-    console.log(loaderRef)
     Aos.init({duration: 1000});
-    scroll();
   }, [])
 
-  
-  if (!loader){
-    return (
-      <CountUp
-        start={0}
-        end={100}
-        delay={1}
-        duration={10}
-        useEasing={true}
-        useGrouping={true}
-        suffix={'%'}
-        onEnd={ endLoader }
-        >
-      {({ countUpRef }) => (
-        <div className="preloader-container">
-          <h1
-            className={ loaderClass }
-            ref={ countUpRef } />
-          <div className='name-container'>
-            <h1 className={ loaderClass2 }>AGOSTINA M.</h1>
-            <h1 className={ loaderClass2 }>CHILO DIAZ</h1>
-          </div>
-          <div className="progress-container">
-            <div className="progress-bar"/>
-          </div>
-        </div>
-      )}
-    </CountUp>
-    )
-  } else {
     return (
       <div id="main-container"
       data-scroll-container
@@ -95,6 +50,5 @@ function App() {
       </div>
     )
   }
-}
 
 export default App
